056123f13d1f098236fa67b51c66310853df067a
Mahmud Mai <mahmud.mahmudx00@gmail.com>
2023-03-31 01:35:13 +0100

    Feat; add a bunch of middlewares

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
+// middleware imports
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const cookieParser = require('cookie-parser')
+const errorHandler = require('./middleware/errorHandler')
+const { logger } = require('./middleware/logger')
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+
+// middlewareS
+app.use(logger)
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+// built-in middleware
+app.use(express.static('public'))
+app.use(express.json())
+
+
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res)=> {
@@ -16,5 +32,9 @@ app.all('*', (req, res)=> {
         res.type('txt').send('404.html')
     }
 })
+
+
+// errorHandler middleare
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`))
